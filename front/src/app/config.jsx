56557a20ef1cb98813dd53f20356dd7757562303
hcabel <hcabel@student.42.fr>
2021-10-16 12:37:46 +0200

    Make the room page connect to the signaling server

--- front/src/app/config.jsx
+++ front/src/app/config.jsx
@@ -4,7 +4,7 @@ const url = "localhost";
 
 const config = {
 	url_front: `${prefix}://${url}:3000`,
-	url_signaling: `${prefix.replace("http", "ws")}://${url}:3000`
+	url_signaling: `${prefix.replace("http", "ws")}://${url}:8042`
 }
 
 export default config;
--- front/src/app/pages/roomPage/roomPage.jsx
+++ front/src/app/pages/roomPage/roomPage.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
+import config from "../../config";
 
-export default function	Room()
+let SignalingSocket = null;
+
+export default function	RoomPage()
 {
+	const [_LoadingMessage, set_LoadingMessage] = useState("");
+
 	let { roomId } = useParams();
 
-	console.log("ID: ", roomId);
+	useEffect(() => {
+		function	connectClient() {
+			console.log("START");
+			set_LoadingMessage("Connection to the Signaling Server...");
+			if (!window.WebSocket) {
+				set_LoadingMessage("FAILED: Your browser's version is to old.");
+			}
+
+			SignalingSocket = new window.WebSocket(`${config.url_signaling}?roomid=${roomId}`);
+
+			SignalingSocket.onopen = function (event) {
+				set_LoadingMessage("SUCCEEDED: Connection to the Signaling server establish.");
+				console.log(event);
+			};
+		}
+
+		connectClient();
+	}, [roomId]);
+
 	return (
 		<div className="RoomPage">
-			{roomId}
+			{_LoadingMessage}
 		</div>
 	);
 };
