c72c2fd5bec6c13835d7d84ba49dae949b46aac6
hcabel <hcabel@student.42.fr>
2021-10-16 14:58:30 +0200

    Close websocket if the page change (in case your leaving the room)

--- front/src/app/app.jsx
+++ front/src/app/app.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React, {useEffect} from "react";
+import { Switch, Route, Redirect, useHistory } from "react-router-dom";
 
 import LandingPage from "./pages/landingPage/landingPage";
 import RoomPage from "./pages/roomPage/roomPage";
 
 export default function	App()
 {
+	const history = useHistory();
+
+	useEffect(() => {
+		return (history.listen(() => {
+			// This function will be execute every time the Browser route change
+			if (window.SignalingSocket) {
+				if (window.SignalingSocket.readyState === 0 || window.SignalingSocket.readyState === 1) {
+					window.SignalingSocket.close();
+				}
+				window.SignalingSocket = null;
+			}
+
+		}));
+	},[ history ])
+
 	return (
 		<Switch>
 			<Route exact path="/" component={LandingPage} />
