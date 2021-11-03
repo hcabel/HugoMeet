1bd194a2cdb13aa25713dd8886ec2d17333666b7
hcabel <hcabel@student.42.fr>
2021-11-03 23:39:31 +0100

    Create PreRoomPage CSS (Participate button)

--- front/src/app/pages/preRoomPage/preRoomPage.jsx
+++ front/src/app/pages/preRoomPage/preRoomPage.jsx
@@ -6,7 +6,20 @@ function	PreRoomPage() {
 	return (
 		<div className="PreRoomPage">
 			<div className="PRP-Body">
-
+				<div className="PRP-B-Content">
+					<div className="PRP-B-C-Form">
+						<div className="PRP-B-C-F-Title">
+							Prêt à participer ?
+						</div>
+						<div className="PRP-B-C-F-SubmitButtons">
+							<div className="PRP-B-C-F-SB-Participate">
+								<span className="PRP-B-C-F-SB-P-Value">
+									Participer
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
