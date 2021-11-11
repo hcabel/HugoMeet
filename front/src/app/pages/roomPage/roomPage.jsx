ceeee79d43940bf83084a707274523522ca1100a
hcabel <hcabel@student.42.fr>
2021-11-11 15:17:39 +0100

    Mini design improvement in roomPage

--- front/src/app/pages/roomPage/roomPage.jsx
+++ front/src/app/pages/roomPage/roomPage.jsx
@@ -486,16 +486,19 @@ export default function	RoomPage(props) {
 					return (
 						<div key={index} className="RP-VC-Peer">
 							<video className="RP-VC-P-Video" id={`VideoStream_${peer._id}`} src={(_SelfId && peer._id === _SelfId ? window.localStream : (PeersConnection.get(peer._id) ? PeersConnection.get(peer._id).streams : null))} />
-							<div className="RP-VC-P-Overlay">
-								<div className="RP-VC-P-O-Name">
-									{`${peer.name} ${peer._id}`}
-								</div>
-								{peer.isMuted && peer.isMuted === true &&
-									<div className="RP-VC-P-O-AudioStatus">
-										Muted
-									</div>
-								}
+							<div className="RP-VC-P-Name">
+								{peer.name}
 							</div>
+							{peer.isMuted && peer.isMuted === true &&
+								<>
+									<div className="RP-VC-P-AudioStatus">
+										<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="#000000" class="Hdh4hc cIGbvc">
+											<path d="M0 0h24v24H0zm0 0h24v24H0z" fill="none"></path>
+											<path d="M19 11h-1.7c0 .74-.16 1.43-.43 2.05l1.23 1.23c.56-.98.9-2.09.9-3.28zm-4.02.17c0-.06.02-.11.02-.17V5c0-1.66-1.34-3-3-3S9 3.34 9 5v.18l5.98 5.99zM4.27 3L3 4.27l6.01 6.01V11c0 1.66 1.33 3 2.99 3 .22 0 .44-.03.65-.08l1.66 1.66c-.71.33-1.5.52-2.31.52-2.76 0-5.3-2.1-5.3-5.1H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c.91-.13 1.77-.45 2.54-.9L19.73 21 21 19.73 4.27 3z"></path>
+										</svg>
+									</div>
+								</>
+							}
 						</div>
 					);
 				})}
@@ -503,7 +506,9 @@ export default function	RoomPage(props) {
 			{/* BUTTONS UNDER VIDEOS */}
 			<div className="RP-ToolsBox">
 				<div className="RP-TB-Left">
-					{`Welcome to room: ${roomId}`}
+					<div className="RP-TB-L-RoomId">
+						{roomId}
+					</div>
 				</div>
 				<div className="RP-TB-Center">
 					<div className={`RP-TB-C-Button-${_Audio ? "On" : "Off"} Center-Button-MicroStatus`} onClick={() => set_Audio(!_Audio)}>
